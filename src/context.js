aeb187e8d84e51b19922d5725a493ab381ae8d15
mtodorov95 <mtodorov95@gmail.com>
2020-12-07 10:58:53 +0200

    fetch data

--- src/context.js
+++ src/context.js
@@ -6,8 +6,34 @@ const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('a');
   const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try{
+      const response = await fetch(`${url}${search}`);
+      const data = await response.json();
+      const {drinks} = data;
+      if(drinks){
+        const newCocktails = drinks.map((drink) => {
+          const {idDrink, strDrink, strDrinkThumb, strAlcoholic,strGlass} = drink;
+          return {id:idDrink, name:strDrink, image:strDrinkThumb, info:strAlcoholic,glass:strGlass};
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error){
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks();
+  },[search])
 
   return (
   <AppContext.Provider value={{loading, cocktails, setSearch}}>
