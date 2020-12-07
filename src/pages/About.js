c22d91212bdf804c4660f9a396075033a2974d38
mtodorov95 <mtodorov95@gmail.com>
2020-12-07 10:28:53 +0200

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in gravida dolor, ac imperdiet est. Aenean ullamcorper dapibus mollis. Sed id orci nec arcu tristique eleifend sodales vel nisi. Cras at est nec quam facilisis volutpat nec in magna. Duis egestas mi non dictum vulputate. In pulvinar consequat placerat. Nam elementum pellentesque nulla, in suscipit felis congue non. Quisque eleifend tempus massa sed vulputate. Maecenas sit amet varius risus. Aenean nibh sem, tincidunt at quam a, eleifend fermentum libero. Integer eu massa maximus, ornare augue iaculis, feugiat urna. Sed et dignissim elit, eget accumsan mauris. Nulla laoreet tempus tortor. Nam in felis hendrerit, tempor metus at, dictum diam. Aliquam suscipit facilisis venenatis. Fusce dictum feugiat neque.
+      </p>
+    </section>
   )
 }
 
