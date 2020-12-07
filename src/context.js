8b84fc8e04bcae0f93026ffce2be5b5419a2324e
mtodorov95 <mtodorov95@gmail.com>
2020-12-07 10:44:10 +0200

    loading

--- src/context.js
+++ src/context.js
@@ -5,9 +5,17 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [cocktails, setCocktails] = useState([]);
+
+  return (
+  <AppContext.Provider value={{loading, cocktails, setSearch}}>
+    {children}
+    </AppContext.Provider>
+    )
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
